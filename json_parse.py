49def2b446669508ab6f6b153793f23609990bde
chrishna1 <adjnct@gmail.com>
2018-09-26 15:57:13 +0530

    Update json_parse.py

--- json_parse.py
+++ json_parse.py
@@ -11,15 +11,18 @@ file.close()
     
 #Now work with json_data
 
-# Open/Create a file to append data
-csvFile = open('tweet_df.csv', 'a')
-#Use csv Writer
-csvWriter = csv.writer(csvFile)
+with open('tweets.csv','w') as csvFile:
+    #Use csv Writer
+    csvWriter = csv.writer(csvFile)
 
-df = pd.read_csv(file)
-    for serialNo,tweet in enumerate(json_data):
-        csvWriter.writerow(serialNo,tweet['id'],tweet['text'])
-    
+    #write header
+    csvWriter.writerow(['S.no', 'tweetid', 'tweettext']) #writerow accepts a list of values.
+
+    df = pd.read_csv(file)
+        for serialNo,tweet in enumerate(json_data):
+            csvWriter.writerow([serialNo,tweet['id'],tweet['text']]) #writerow accepts a list of values.
+
+            
 #reading csv
-tweets = pd.read_csv('tweet_df.csv')
+tweets = pd.read_csv('tweets.csv')
 print(tweets.head(5))
